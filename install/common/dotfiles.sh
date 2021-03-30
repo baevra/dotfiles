9fffc66c3c9da169d23e70411909f43ba298cbbf
Roman Baev <baevra@gmail.com>
2021-03-30 17:54:25 +0300

    Update

--- install/common/dotfiles.sh
+++ install/common/dotfiles.sh
@@ -2,9 +2,13 @@
 
 echo "cloning dotfiles"
 git clone https://github.com/baevra/dotfiles.git "${HOME}/dotfiles"
+
+# configs
 ln -sf "${HOME}/dotfiles/configs/.zsh" "${HOME}/.config"
 ln -sf "${HOME}/dotfiles/configs/.gitignore_global" "${HOME}/.gitignore_global"
 ln -sf "${HOME}/dotfiles/configs/.gitconfig" "${HOME}/.gitconfig"
 ln -sf "${HOME}/dotfiles/configs/.hyper.js" "${HOME}/.hyper.js"
-ln -sf "${HOME}/dotfiles/configs/xcode/KeyBindings/baevra.idekybindings" "${HOME}/Library/Developer/Xcode/KeyBindings/baevra.idekybindings"
-ln -sf "${HOME}/dotfiles/configs/xcode/FontAndColorThemes/Default\ (Dark).xccolortheme" "${HOME}/Library/Developer/Xcode/FontAndColorThemes/Default\ (Dark).xccolortheme"
+
+# xcode
+mkdir -p "${HOME}/Library/Developer/Xcode/UserData/KeyBindings"
+ln -sf "${HOME}/dotfiles/configs/xcode/KeyBindings/baevra.idekeybindings" "${HOME}/Library/Developer/Xcode/UserData/KeyBindings/baevra.idekeybindings"
